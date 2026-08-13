0a75c60015ce78e325c0f859dd929931c283ae92
DanaShaynan <danashinan2020@gmail.com>
2026-08-14 02:15:50 +0300

    Create solution.py

---
+++ Task-01/solution.py
@@ -0,0 +1,17 @@
+# Swap two variables
+
+A = 30
+B = 50
+
+print("Before swap:")
+print("A =", A)
+print("B =", B)
+
+# Using third variable
+temp = A
+A = B
+B = temp
+
+print("\nAfter swap:")
+print("A =", A)
+print("B =", B)
